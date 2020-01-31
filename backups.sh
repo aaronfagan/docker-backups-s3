44b9cf786b3854fe67000085b4eddfb7ec6fcb54
Aaron Fagan <aaron.fagan@telus.com>
2020-01-31 11:22:35 -0700

    Update backups.sh

--- backups.sh
+++ backups.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DATE=`date +%Y-%m-%d`
 DIR_TEMP="/tmp/docker-backups"
+LABEL=$(hostname)
 
 for FOLDER in /data/*; do
 	NAME=${FOLDER//\/data\//}
@@ -8,15 +9,15 @@ for FOLDER in /data/*; do
 	(
 		set -e
 		DIR_TEMP="${DIR_TEMP}/${NAME}_`date +%s`"
-		FILENAME=$(echo ${NAME}_${DATE}_${HOSTNAME} | tr A-Z a-z)
+		FILENAME=$(echo ${NAME}_${DATE}_${LABEL} | tr A-Z a-z)
 		FILENAME=${FILENAME// /-}
 		FILENAME="${FILENAME//./-}.tar.gz"
 		mkdir -p "${DIR_TEMP}"
 		tar -zcf "${DIR_TEMP}/${FILENAME}" -C "${FOLDER}" .
-		aws s3 mv "${DIR_TEMP}/${FILENAME}" "${S3_PATH}/${HOSTNAME}/${DATE}/${FILENAME}" --quiet
+		aws s3 mv "${DIR_TEMP}/${FILENAME}" "${S3_PATH}/${LABEL}/${DATE}/${FILENAME}" --quiet
 	)
 	if [ "$?" -ne "0" ]; then
-		pushover --title "${HOSTNAME}" --message "Cronjob failed @ `date +"%r"`\n[$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")]" --priority 1 -q
+		pushover --title "${LABEL}" --message "Cronjob failed @ `date +"%r"`\n[$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")]" --priority 1 -q
 		rm -rf "${DIR_TEMP}/${NAME}_"*
 		echo -ne "failed!\n"
 	else
