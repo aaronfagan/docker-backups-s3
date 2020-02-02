99c85fa3f1454ceb2a3df27fcd414a58266f6694
Aaron Fagan <aaron.fagan@telus.com>
2020-02-01 19:39:37 -0700

    Update backups.sh

--- backups.sh
+++ backups.sh
@@ -1,20 +1,80 @@
 #!/bin/bash
-DATE=`date +%Y-%m-%d_%H-%M-%S`
-DIR_TEMP="/tmp/docker-backups"
 
-for FOLDER in /data/*; do
-	FOLDER_NAME=${FOLDER//\/data\//}
-	echo -ne "[$(date +'%F %T')] Backing up ${FOLDER}..."
-	(
-		set -e
-		DIR_TEMP="${DIR_TEMP}/${FOLDER_NAME}_${DATE}"
-		FILENAME="$(echo ${FOLDER_NAME}_${APP_NAME}_${DATE} | tr A-Z a-z)"
-		FILENAME="${FILENAME// /-}"
-		FILENAME="${FILENAME//./-}.tar.gz"
-		mkdir -p "${DIR_TEMP}"
-		tar -zcf "${DIR_TEMP}/${FILENAME}" -C "${FOLDER}" .
-		/usr/bin/aws s3 mv "${DIR_TEMP}/${FILENAME}" "${S3_PATH}/${APP_NAME}/${DATE}/${FILENAME}" --quiet
-		rm -rf "${DIR_TEMP}"
-	)
-	[ "$?" -ne "0" ] && echo -ne "failed!\n" || echo -ne "success!\n"
+usage() {
+	echo "
+Usage:
+    ./$(basename "$0") [options]
+
+Options:
+    --app-name     The name of your app. REQUIRED.
+    --dir-backup   The directory to backup. REQUIRED.
+    --s3-path      The full Amazon S3 bucket path. REQUIRED.
+
+Example:
+    ./$(basename "$0") \\
+    --app-name my-app \\
+    --dir-backup /data \\
+    --s3-path s3://my-bucket/backups
+"
+}
+
+while [[ $# -gt 0 ]]; do
+	KEY="$1"
+	case ${KEY} in
+		--app-name)
+			APP_NAME="${2:-$APP_NAME}"
+			shift
+			shift
+		;;
+
+		--dir-backup)
+			DIR_BACKUP="${2:-$DIR_BACKUP}"
+			shift
+			shift
+		;;
+
+		--s3-path)
+			S3_PATH="${2:-$S3_PATH}"
+			shift
+			shift
+		;;
+		--help)
+			usage
+			exit 0
+		;;
+		*) 
+			INVALID+=("$1")
+			shift
+			echo -e "\033[0;31mERROR:\033[0;37m Invalid arguement: $INVALID\033[0m"
+			usage
+			exit 0
+		;;
+	esac
 done
+
+if [ -z "${APP_NAME}" ] || [ -z "${DIR_BACKUP}" ] || [ -z "${S3_PATH}" ]; then
+	if [ -z "${APP_NAME}" ]; then echo -ne "\033[0;31mERROR:\033[0;37m --app-name arguement is required.\033[0m\n"; fi
+	if [ -z "${DIR_BACKUP}" ]; then echo -ne "\033[0;31mERROR:\033[0;37m --dir-backup arguement is required.\033[0m\n"; fi
+	if [ -z "${S3_PATH}" ]; then echo -ne "\033[0;31mERROR:\033[0;37m --s3-path arguement is required.\033[0m\n"; fi
+	usage
+	exit 0
+else
+	DATE=`date +%Y-%m-%d`
+	DIR_TEMP="/tmp/docker-backups"
+	for DIR in ${DIR_BACKUP}/*; do
+		DIR_NAME=${DIR//\/data\//}
+		echo -ne "[$(date +'%F %T')] Backing up ${DIR}..."
+		(
+			set -e
+			DIR_TEMP="${DIR_TEMP}/${DIR_NAME}_${DATE}"
+			FILENAME="$(echo ${DIR_NAME}_${APP_NAME}_${DATE} | tr A-Z a-z)"
+			FILENAME="${FILENAME// /-}"
+			FILENAME="${FILENAME//./-}.tar.gz"
+			mkdir -p "${DIR_TEMP}"
+			tar -zcf "${DIR_TEMP}/${FILENAME}" -C "${DIR}" .
+			/usr/bin/aws s3 mv "${DIR_TEMP}/${FILENAME}" "${S3_PATH}/${APP_NAME}/${DATE}/${FILENAME}" --quiet
+			rm -rf "${DIR_TEMP}"
+		)
+		[ "$?" -ne "0" ] && echo -ne "failed!\n" || echo -ne "success!\n"
+	done
+fi
