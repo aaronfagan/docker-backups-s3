dc45bc87863eea8395f9ad45eac0bf0d8e74c08f
Aaron Fagan <aaron.fagan@telus.com>
2020-03-19 21:39:28 -0600

    Update backups.sh

--- backups.sh
+++ backups.sh
@@ -78,7 +78,7 @@ else
 			tar -zcf "${DIR_TEMP}/${FILENAME}" -C "${DIR}" .
 			/usr/bin/aws s3 cp "${DIR_TEMP}/${FILENAME}" "${S3_PATH}/${DATE}/${APP_NAME}/${FILENAME}" --quiet
 			if [ "${CREATE_LATEST}" ]; then
-				/usr/bin/aws s3 cp "${S3_PATH}/${DATE}/${APP_NAME}/${FILENAME}" "${S3_PATH}/latest/${FILENAME_LATEST}" --quiet
+				/usr/bin/aws s3 cp "${S3_PATH}/${DATE}/${APP_NAME}/${FILENAME}" "${S3_PATH}/latest/${APP_NAME}/${FILENAME_LATEST}" --quiet
 			fi
 			rm -rf "${DIR_TEMP}"
 		)
