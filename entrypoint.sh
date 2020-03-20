17526a59b0bd9a192cb1d2a989c3019f000bbfe7
Aaron Fagan <aaron.fagan@telus.com>
2020-03-19 23:16:15 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,11 +31,7 @@ dpkg-reconfigure -f noninteractive tzdata > /dev/null 2>&1
 aws configure set default.region ${AWS_DEFAULT_REGION}
 aws configure set default.output json
 
-if [ "$(echo ${CREATE_LATEST} | tr A-Z a-z)" == "true" ]; then
-    echo "${CRON} root /root/backups.sh --app-name '${APP_NAME}' --dir-backup '${DIR_BACKUP}' --s3-path '${S3_PATH}' --create-latest > /proc/1/fd/1" > /etc/cron.d/backup
-else
-    echo "${CRON} root /root/backups.sh --app-name '${APP_NAME}' --dir-backup '${DIR_BACKUP}' --s3-path '${S3_PATH}' > /proc/1/fd/1" > /etc/cron.d/backups
-fi
+echo "${CRON} root /root/backups.sh --app-name '${APP_NAME}' --dir-backup '${DIR_BACKUP}' --s3-path '${S3_PATH}' $([ "$(echo ${CREATE_LATEST} | tr A-Z a-z)" == "true" ] && echo "--create-latest" || echo -e "\b") > /proc/1/fd/1" > /etc/cron.d/backups
 
 /etc/init.d/cron start > /dev/null 2>&1
 
